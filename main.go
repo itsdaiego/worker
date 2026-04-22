9210b87751468c4740902e1825876b0a7c502510
Dyego Rodrigo <dyego9444@gmail.com>
2026-04-22 17:38:24 -0300

    add routes

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+
+	. "challenge/model"
+	. "challenge/repository"
 )
 
 type Server struct {
@@ -29,6 +35,65 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	s.router.GET("/jobs", func(c *gin.Context) {
+		var jobs []Job
+
+		jobRepo := NewJobRepository(s.db)
+
+		jobs, err := jobRepo.GetAllJobs()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch jobs"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"jobs": jobs})
+	})
+
+	s.router.GET("/jobs/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		jobRepo := NewJobRepository(s.db)
+
+		intId, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid job ID"})
+			return
+		}
+
+		job, err := jobRepo.GetJobById(intId)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch job"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"job": job})
+	})
+
+	s.router.POST("/jobs", func(c *gin.Context) {
+		var jobRequest CreateJobRequest
+		jsonData, err := c.GetRawData()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		err = json.Unmarshal(jsonData, &jobRequest)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse job data"})
+			return
+		}
+
+		jobRepo := NewJobRepository(s.db)
+
+		createdJob, err := jobRepo.CreateJob(jobRequest)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create job"})
+			return
+		}
+
+		c.JSON(http.StatusOK, createdJob)
+	})
 
 	if err := s.router.Run(":8080"); err != nil {
 		log.Fatal(err)
---
+++ model/job.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Job struct {
+	ID      string `json:"id" gorm:"type:uuid;primaryKey"`
+	Type    string `json:"type"`
+	Payload string `json:"payload"`
+	Status  string `json:"status"`
+}
+
+type CreateJobRequest struct {
+	Type    string `json:"type"`
+	Payload string `json:"payload"`
+}
+
+func (job *Job) BeforeCreate(tx *gorm.DB) error {
+	job.ID = uuid.New().String()
+	return nil
+}
